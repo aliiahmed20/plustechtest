1e33a0d3d7af04d10dfabddb04e56eb91d848c40
aliiahmed20 <99345405+aliiahmed20@users.noreply.github.com>
2023-10-15 23:29:23 +0300

    Add files via upload

---
+++ contact_chatter/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Contact Chatter",
+
+    'summary': """
+        adding tracking for the contact form view
+       """,
+
+    'description': """
+        tracking for the contact form view
+    """,
+    'author': "Plus Tech",
+    'website': "",
+    'category': 'sale',
+    'version': '0.3',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale', 'mail', 'stock', 'account'],
+
+    'data': [
+    ],
+
+}
